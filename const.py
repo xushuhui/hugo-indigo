3123b33a669e5545545a0c0b1418c0b086e865b7
xushuhui <474497097@qq.com>
2021-07-06 22:29:40 +0800

    feat: headers

---
+++ const.py
@@ -0,0 +1,20 @@
+headers = {
+        'Host': 'gate.lagou.com',
+        'Connection': 'keep-alive',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'sec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+        'Accept': 'application/json, text/plain, */*',
+        'Authorization': '50dc5543e28179e1b1f19b2b33dd822d4a77358f1ad63131',
+        'X-L-REQ-HEADER': '{"deviceType":1,"userToken":"50dc5543e28179e1b1f19b2b33dd822d4a77358f1ad63131"}',
+        'sec-ch-ua-mobile': '?0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
+        'Origin': 'https://edu.lagou.com',
+        'Sec-Fetch-Site': 'same-site',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        'Referer': 'https://edu.lagou.com/',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Cookie': ''
+     }
--- crawl_list.py
+++ crawl_list.py
@@ -1,31 +1,12 @@
 #encoding:utf-8
 import requests
-
+from const import headers 
 
 # 获取所有专栏并且一键订阅所有未购买专栏
 def lessions_subscription():
     lession_url = 'https://gate.lagou.com/v1/neirong/edu/homepage/getCourseListV2?isPc=true'
     pay_url = 'https://gate.lagou.com/v1/neirong/edu/member/drawCourse?courseId={id}'
-    headers = {
-        'Host': 'gate.lagou.com',
-        'Connection': 'keep-alive',
-        'Pragma': 'no-cache',
-        'Cache-Control': 'no-cache',
-        'sec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
-        'Accept': 'application/json, text/plain, */*',
-        'Authorization': '50dc5543e28179e1b1f19b2b33dd822d4a77358f1ad63131',
-        'X-L-REQ-HEADER': '{"deviceType":1,"userToken":"50dc5543e28179e1b1f19b2b33dd822d4a77358f1ad63131"}',
-        'sec-ch-ua-mobile': '?0',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
-        'Origin': 'https://edu.lagou.com',
-        'Sec-Fetch-Site': 'same-site',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Dest': 'empty',
-        'Referer': 'https://edu.lagou.com/',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-        'Cookie': ''
-   }
+    
    
     response = requests.get(url=lession_url, headers=headers)
     lessions_all = []
