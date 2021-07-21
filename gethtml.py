45e03cd809e31fe802774d2f08280857858b7077
xushuhui <474497097@qq.com>
2021-07-21 18:18:57 +0800

    up

--- gethtml.py
+++ gethtml.py
@@ -1,15 +1,23 @@
 from requests_html import HTMLSession 
 import h2md
-def get_html(): 
+def get_list():
     session = HTMLSession()
     r = session.get('http://www.imooc.com/wiki/javalesson/operators.html')
+    content =  r.html.find('.jie',first = True)
+    links = list(content.links)
+    
+    get_html(links[0],content.text)
+
+def get_html(url,name): 
+    session = HTMLSession()
+    r = session.get('http://www.imooc.com/'+url)
     content = r.html.find('.content',first = True)
-    file = 'operators.md'
+    file = "imooc/"name+'.md'
     
     text = h2md.convert(content.html)
     file_data = ""
     # 写入处理后的内容
-    with open(file, 'w') as f:
+    with open(file, 'w',encoding="utf-8") as f:
         f.write(text)
     
     for line in open(file): 
@@ -21,4 +29,4 @@ def get_html():
 
 
 if __name__ == '__main__':
-    get_html()
+    get_list()
